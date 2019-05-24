14a3c5f9f87713235147cd007a11c9fe2221fbfc
Matheus Bordin <mabordin98@gmail.com>
2019-05-24 13:44:36 -0300

    Add game route

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,17 @@ import * as http from "http";
 import * as express from "express";
 import * as socket from "socket.io";
 import * as cors from "cors";
+import * as path from "path";
 import config from "./config";
 import { SocketService } from './services/socket';
 import { GameService } from "./services/game";
 
 const app = express();
 app.use(cors());
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 app.get('/heartbeat', (req, res) => res.send('Alive!'));
 
 const server = http.createServer(app);
